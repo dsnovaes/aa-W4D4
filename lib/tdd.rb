7b76a18803a7933ae98f21cf36cfe8ba5715470d
Diego Novaes <dsnovaes1@gmail.com>
2022-10-27 15:11:29 -0700

    until two_sum

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -1,3 +1,29 @@
 class Array
-    
+
+    def my_uniq
+        unique = []
+        self.each do |ele|
+            unless unique.include?(ele)
+                unique << ele
+            end
+        end
+        unique
+    end
+
+    def two_sum
+        summed = []
+        (0...self.length).each do |i|
+            (0...self.length).each do |j|
+                if j > i && self[i] + self[j] == 0
+                    summed << [i,j]
+                end
+            end
+        end
+        summed
+    end
+
 end
+
+# p [1,1,2,2,3,4,4].my_uniq
+
+# p [-1, 0, 2, -2, 1].two_sum
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -4,15 +4,34 @@ require "rspec"
 describe Array do
     
     describe "#my_uniq" do 
-        subject(:original_array) { [1,1,2,2,3,4,4] }
+        subject(:original_array) { Array.new([1,1,2,2,3,4,4]) }
 
-        it "should take in an array as an argument" do
-            expect(self).to be_an(Array)
+        it "should be called on an array" do
+            expect(original_array).to be_an(Array)
         end
 
-        it "should return an array with unique elements" do
-            expect(:original_array.my_uniq).to eq([1,2,3,4])
+        it "should return a new array with unique elements" do
+            expect(original_array.my_uniq).to eq([1,2,3,4])
         end
 
     end
+
+    describe "#two_sum" do 
+        subject(:original_array) { Array.new([-1, 0, 2, -2, 1]) }
+
+        it "should be called on an array" do
+            expect(original_array).to be_an(Array)
+        end
+
+        it "should return a 2D array with pairs of indices from the original array that sum to zero" do
+            expect(original_array.two_sum).to eq([[0, 4], [2, 3]])
+        end
+
+        it "should return smaller elements first" do
+            expect(original_array.two_sum).not_to eq([[4,0], [3,2]])
+        end
+
+    end
+
 end
+
